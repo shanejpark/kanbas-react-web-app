e402576ba9e9d7201ab61befe4605985b281eb3b
Shane Park <shanejpark@gmail.com>
2023-11-02 12:03:26 -0400

    completed labs

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import ConditionalOutput from "./ConditionalOutput";
 import DynamicStyling from "./DynamicStyling";
 import JavaScript from "./JavaScript";
@@ -6,9 +7,18 @@ import Styles from "./Styles";
 import TodoList from "./todos/TodoList";
 
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+
       <TodoList />
       <ConditionalOutput />
       <Styles />
